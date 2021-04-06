d5a2c524a72faf3cf03e83c7cd06e5480d13eae8
zhipeng <i@mezzp.com>
2021-04-06 17:19:18 +0800

    :art:小修改

--- exp1/src/jk20/zhaozhipeng38/servlet/LoginServlet.java
+++ exp1/src/jk20/zhaozhipeng38/servlet/LoginServlet.java
@@ -12,26 +12,27 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-      
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String userName = request.getParameter("userName");
 		String userPwd = request.getParameter("userPwd");
-		//编码转换，否则汉字乱码
-		String gender = new String(request.getParameter("gender").getBytes("iso-8859-1"), "utf-8");	
+		// 编码转换，否则汉字乱码
+		String gender = new String(request.getParameter("gender").getBytes("iso-8859-1"), "utf-8");
 		System.out.println(gender);
-		if(userName.equals("admin")&&userPwd.equals("1234")&&gender.equals("男")) {
-			//用请求重定向
+		if (userName.equals("admin") && userPwd.equals("1234") && gender.equals("男")) {
+			// 用请求重定向
 			response.sendRedirect("success.html");
 			System.out.println("----ok----");
-		
-		}else {
-			//用请求转发
+
+		} else {
+			// 用请求转发
 			request.getRequestDispatcher("failure.html").forward(request, response);
 			System.out.println("----error----");
 		}
 	}
-
 }
