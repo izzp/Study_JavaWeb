67c3939710d4cd2a45c888baf9bcfa652b372f6c
zhipeng <i@mezzp.com>
2021-04-07 18:52:14 +0800

    :art: IDEA更新202103之后终于可以写servlet了。。。莫名其妙

--- HelloTomcat/src/com/mezzp/servlet/HelloServlet.java
+++ HelloTomcat/src/com/mezzp/servlet/HelloServlet.java
@@ -1,9 +1,24 @@
 package com.mezzp.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author zhaozhipeng
  * @version 1.0
  * @date 2021/4/6
  */
-public class HelloServlet  {
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
